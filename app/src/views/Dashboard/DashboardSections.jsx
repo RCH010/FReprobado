d40988601f27cb6889e30cb8952842d4060d7a53
Sergio Diosdado <sgdc_98@hotmail.com>
2021-11-13 11:07:21 -0600

    Initial evaluation form

--- app/src/views/Dashboard/DashboardSections.jsx
+++ app/src/views/Dashboard/DashboardSections.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { tabsOptions } from '../../utils/menuItems';
 import { DashboardContext } from '../../providers/DashboardProvider';
+import { EvaluationForm } from './EvaluationForm';
 
 export const DashboardSections = () => {
   const {currentTab} = useContext(DashboardContext)
   switch (currentTab) {
     case tabsOptions.NEWEVALUATION:
       return (
-        <h1>ESTA ES LA DE NEW EVALUATION</h1>
+        EvaluationForm()
       )
     case tabsOptions.PASTEVALUATIONS:
       return (
